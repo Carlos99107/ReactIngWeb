365e263c7f6627b4fd968261600954130f80dcdb
Carlos <carlossmp9910@outlook.com>
2021-07-24 18:29:12 -0500

    Subiendo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import JSONDATA from './Data.json';
+import { useState } from 'react';
+import {motion} from 'framer-motion';
 
 function App() {
+  const[searchTerm, setSearchTerm]=useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.input type="text" placeholder="Buscar" onChange={event=>{setSearchTerm(event.target.value)}}
+       initial={{x: "200vw", y:"500px"}}
+       animate={{
+         x:"0",
+         y:"0",
+         transition:{duration: 1, ease: "easeInOut"}
+       }}/>
+      {JSONDATA.filter((val)=>{
+        if(searchTerm ==""){
+          return val
+        }else if (val.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())){
+          return val
+        }
+      }).map((val, key)=>{
+          return (
+            <motion.div className="user" key={key}>
+              <motion.p
+               initial={{x: "200vw", y:"500px"}}
+               animate={{
+                 x:"0",
+                 y:"0",
+                 transition:{duration: 1, ease: "easeInOut"}
+               }}>
+               
+                {val.name}</motion.p>
+              
+            </motion.div>
+             );
+      })}
+  
     </div>
   );
 }
